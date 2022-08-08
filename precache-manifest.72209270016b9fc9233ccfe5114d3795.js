d35bbcd05700cb1c266328b3dc6b4e398a3d551a
Eozinum <eozinum@gmail.com>
2022-08-08 15:38:37 +0300

    Updates

---
+++ precache-manifest.72209270016b9fc9233ccfe5114d3795.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ca086a31bbe28723138",
+    "url": "/static/css/main.3328da0d.chunk.css"
+  },
+  {
+    "revision": "1ca086a31bbe28723138",
+    "url": "/static/js/main.1ca086a3.chunk.js"
+  },
+  {
+    "revision": "14b060c7915162ae1f5f",
+    "url": "/static/js/1.14b060c7.chunk.js"
+  },
+  {
+    "revision": "9987dd54f14d0edf957d",
+    "url": "/static/js/2.9987dd54.chunk.js"
+  },
+  {
+    "revision": "c8dbfd972bc17939af0e",
+    "url": "/static/js/runtime~main.c8dbfd97.js"
+  },
+  {
+    "revision": "a2d061dd051ee2170e89757228686a90",
+    "url": "/index.html"
+  }
+];
